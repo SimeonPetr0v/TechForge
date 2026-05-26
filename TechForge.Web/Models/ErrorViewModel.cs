5d2559c1fc1c734b68df2549223f00f9e6c4b066
SimeonPetr0v <simeonp925@gmail.com>
2026-05-26 19:28:19 +0300

    Add project files.

---
+++ TechForge.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechForge.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+}
---
+++ TechForge.Web/wwwroot/js/site.js
@@ -0,0 +1,5 @@
+// TechForge global UI helpers - extended in later stages
+(function () {
+    'use strict';
+    window.TechForge = window.TechForge || {};
+})();
